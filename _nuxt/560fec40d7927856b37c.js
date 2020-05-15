7c7d5cd35f1fe78b17bbef31b04c599615de6c02
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-15 11:32:29 +0000

    Deploy TheLearneer/santoshb.com.np to github.com/TheLearneer/santoshb.com.np.git:gh-pages

---
+++ _nuxt/560fec40d7927856b37c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[13],{235:function(e,n,t){"use strict";t.r(n);var l={middleware:"page"},r=t(60),component=Object(r.a)(l,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
